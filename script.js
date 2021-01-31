70bcdde47ee99008658e4bccd247a9b01c7efb6a
schirmann <cshirmann.dev@gmail.com>
2021-01-31 17:42:32 -0300

    adicionando ao formulario a opçao close

--- script.js
+++ script.js
@@ -86,13 +86,14 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement('tr')
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+    tr.dataset.index = index 
 
     DOM.transactionsContainer.appendChild(tr)
     
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
     const amount =  Utils.formatCurrency(transaction.amount)
@@ -103,7 +104,7 @@ const DOM = {
     <td class="${CSSclass}">${amount}</td>
     <td class="date">${transaction.date}</td>
     <td>
-      <img src="./assets/minus.svg" alt="Remover Transação">
+      <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transação">
     </td>
       `
   
@@ -217,7 +218,7 @@ const Form = {
     Transaction.add(transaction)
     Form.clearFields()
     Modal.close()
-    App.reload()
+   
 
     } catch (error) {
       alert(error.message)
@@ -232,9 +233,8 @@ const App = {
   init() {
 
 
-    Transaction.all.forEach(transaction => {
-      DOM.addTransaction(transaction)
-    })
+    Transaction.all.forEach(DOM.addTransaction) 
+  
     
     DOM.updateBalance()
     
